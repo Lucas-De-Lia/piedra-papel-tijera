ea2e8360398bbd3f43584b620705b6cae1a36cbd
Lucas De Lia <lucasdelia018@gmail.com>
2024-04-25 10:13:35 -0300

    Modificacion html y avance Js, falta terminar logica del juego y restart

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
+//funciones
 function random(){
     let numeroRandom= Math.random();
     let nuevoNumero=(Math.floor((numeroRandom*10))%3)+1;
     return nuevoNumero;
 }
+//objetos del DOM a modificar y/o esperar Evento 
+const contenedor=document.querySelector('.contenedor');
+const h1=document.querySelector('.contenedor h1');
+const contador=document.getElementById('contador');
+const eleccionUsuario= document.getElementsByClassName('button-game');
+const restart= document.getElementById('restart');
+//eventos del DOM 
+let acumuladorResultados=[0,0];
+
+eleccionUsuario[0].addEventListener('click',()=>{const eleccionMaquina=random();
+    if(eleccionMaquina===1){
+        //empate
+        const div=document.createElement('div');
+        div.classList.add('resultado');
+        div.innerText=`IT'S A DRAW 😱​`
+        contenedor.insertBefore(h1,div);
+    } else if(eleccionMaquina===2){
+        const div=document.createElement('div');
+        div.classList.add('resultado');
+        div.innerText='OH NOO, YOU LOSE YOUR OPONENT CHOOSE PAPER!';
+        contenedor.insertBefore(h1,div);
+        acumuladorResultados[1]+=1;
+        
+        contador.innerText=`${acumuladorResultados[0]}:${acumuladorResultados[1]}`;
+    }
+});
+eleccionUsuario[0].addEventListener('keydown',);
+eleccionUsuario[1].addEventListener('click',);
+eleccionUsuario[1].addEventListener('keydown',);
+eleccionUsuario[2].addEventListener('click',);
+eleccionUsuario[2].addEventListener('keydown',);
+
+
+
+
+/*  
+
 //el usuario eligio piedra
 function piedra(){
     const eleccionMaquina=random();
@@ -39,3 +77,4 @@ function tijera(){
         alert('Empate!,la maquina eligio Tijera')
     }
 }
+*/
