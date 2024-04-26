7c1bec5f1a76ab65baabbec63cd52f45a08aa594
Lucas De Lia <lucasdelia018@gmail.com>
2024-04-25 23:08:51 -0300

    terminar funciones juego

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ function random(){
     let nuevoNumero=(Math.floor((numeroRandom*10))%3)+1;
     return nuevoNumero;
 }
+const restaurar=()=>{
+    const nuevoDiv=document.querySelector('.resultado');
+    if(nuevoDiv!==null){
+        contenedor.removeChild(nuevoDiv);
+        acumuladorResultados[0]=0;
+        acumuladorResultados[1]=0;
+        contador.innerText='0:0'
+    }
+}
+
+const buscarGanador= (eleccionUsuario)=>{
+    if(eleccionUsuario==='piedra'){
+        
+    }else if(eleccionUsuario==='papel'){
+
+    }else{
+
+    }
+}
 //objetos del DOM a modificar y/o esperar Evento 
 const contenedor=document.querySelector('.contenedor');
 const h1=document.querySelector('.contenedor h1');
@@ -11,8 +30,21 @@ const contador=document.getElementById('contador');
 const eleccionUsuario= document.getElementsByClassName('button-game');
 const restart= document.getElementById('restart');
 //eventos del DOM 
-let acumuladorResultados=[0,0];
 
+let acumuladorResultados=[0,0];
+//reinicio
+restart.addEventListener('click',restaurar);
+
+eleccionUsuario[0].addEventListener('click',buscarGanador('piedra'));
+eleccionUsuario[1].addEventListener('click',buscarGanador('papel'));
+eleccionUsuario[2].addEventListener('click',buscarGanador('tijera'));
+
+
+
+
+
+
+/*  
 eleccionUsuario[0].addEventListener('click',()=>{const eleccionMaquina=random();
     if(acumuladorResultados[0]===0 && acumuladorResultados[1]===0){
         if(eleccionMaquina===1){
@@ -70,53 +102,17 @@ eleccionUsuario[0].addEventListener('click',()=>{const eleccionMaquina=random();
         }
     }   
 });
+*/
+
+
+
+
+
+
 /* 
 eleccionUsuario[0].addEventListener('keydown',);
 eleccionUsuario[1].addEventListener('click',);
 eleccionUsuario[1].addEventListener('keydown',);
 eleccionUsuario[2].addEventListener('click',);
 eleccionUsuario[2].addEventListener('keydown',);
-*/
-
-
-
-
-/*  
-
-//el usuario eligio piedra
-function piedra(){
-    const eleccionMaquina=random();
-    if(eleccionMaquina===1){
-        //si es 1 lo tomo como que eligio Piedra
-        alert('Empate!,la maquina eligio piedra');
-    }else if(eleccionMaquina ===2){
-        //caso Papel
-        alert('Perdiste!,la maquina eligio Papel')
-    }else{
-        //caso tijera
-        alert('Ganaste!,la maquina eligio Tijera')
-    }
-}
-//el usuario eligio papel
-function papel(){
-    const eleccionMaquina=random();
-    if(eleccionMaquina===1){
-        alert('Ganaste!,la maquina eligio Piedra')
-    }else if(eleccionMaquina===2){
-        alert('Empate!,la maquina eligio papel');
-    }else{
-        alert('Perdiste!,la maquina eligio Tijera');
-    }
-}
-//el usuario eligio tijera
-function tijera(){
-    const eleccionMaquina=random();
-    if(eleccionMaquina===1){
-        alert('Perdiste!,la maquina eligio Piedra');
-    }else if(eleccionMaquina===2){
-        alert('Ganaste!,la maquina eligio Papel');
-    }else{
-        alert('Empate!,la maquina eligio Tijera')
-    }
-}
 */
